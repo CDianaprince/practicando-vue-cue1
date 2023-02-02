48e0f983fa4a5498a39950c442265e4dec9be51a
unknown <dcarol.prince@gmail.com>
2023-02-02 17:38:36 -0600

    final

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
 
-function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement('li');
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-  inputEl.value = '';
-}
+//Creacion de aplicacion
+Vue.createApp({
+  //Definir data
+  data() {
+    return {
+      goals: [],
+      enteredValue:'',//conectarlo con html
 
-buttonEl.addEventListener('click', addGoal);
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = '';
+
+    }//metodo para agregar goals
+  }
+  //Renderizando informacion
+}).mount('#app');
+
+
+
+
+
+//const buttonEl = document.querySelector('button');
+//const inputEl = document.querySelector('input');
+//const listEl = document.querySelector('ul');
+//
+//function addGoal() {
+//  const enteredValue = inputEl.value;
+//  const listItemEl = document.createElement('li');
+//  listItemEl.textContent = enteredValue;
+//  listEl.appendChild(listItemEl);
+//  inputEl.value = '';
+//}
+//
+//buttonEl.addEventListener('click', addGoal);
+//
